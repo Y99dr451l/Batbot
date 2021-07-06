e107bc54fef9a0d68e227bdd7c29f71bb8d60da5
Y99dr451l <matthias@kockisch.eu>
2021-07-06 22:00:33 +0200

    added app arguments

--- keep_alive.py
+++ keep_alive.py
@@ -8,7 +8,7 @@ def home():
     return "I'm alive"
 
 def run():
-  app.run(host='0.0.0.0',port=8080)
+  app.run(host='0.0.0.0',port=8080, use_reloader=False, debug=True)
 
 def keep_alive():
     t = Thread(target=run)

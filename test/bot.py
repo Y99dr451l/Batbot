d5d7cedc99e8e0b73e5ba1714607672c5d5f8557
Y99dr451l <matthias@kockisch.eu>
2021-07-16 14:01:20 +0200

    moved initial extension loader

--- test/bot.py
+++ test/bot.py
@@ -15,10 +15,6 @@ async def on_ready():
     print(f'Bot is almost ready.')
     for guild in client.guilds:
         print(f'On {guild} (id {guild.id})')
-    for filename in os.listdir('./cogs'):
-        if filename.endswith('.py'):
-            client.load_extension(f'cogs.{filename[:-3]}')
-            print(f'Loaded {filename}')
     await client.change_presence(activity=discord.Game('currently testing'), status=discord.Status.idle)
     print(f'Bot is ready and logged in as {client.user}'.format(client))
 
@@ -26,7 +22,7 @@ async def on_ready():
 async def on_member_remove(member):
     print(f'{member} has left a server.')
 
-@client.command
+@client.command()
 async def ping(ctx):
     await ctx.send(f'{client.latency*1000}ms')
 
@@ -35,17 +31,17 @@ def is_admin():
     def predicate(ctx):
         return ctx.message.author.id in admins
 
-@client.command
+@client.command()
 @commands.check(is_admin)
 async def load(ctx, extension):
     client.load_extension(f'cogs.{extension}')
 
-@client.command
+@client.command()
 @commands.check(is_admin)
 async def unload(ctx, extension):
     client.unload_extension(f'cogs.{extension}')
 
-@client.command
+@client.command()
 @commands.check(is_admin)
 async def reload(ctx, extension):
     client.unload_extension(f'cogs.{extension}')
@@ -56,6 +52,12 @@ async def reload(ctx, extension):
 async def on_command_error(ctx, error):
     if isinstance(error, commands.CommandNotFound):
         await ctx.send('Invalid command used.')
+
+# LOAD COGS
+for filename in os.listdir('./cogs'):
+    if filename.endswith('.py'):
+        client.load_extension(f'cogs.{filename[:-3]}')
+        print(f'Loaded {filename}')
 
 keep_alive()
 client.run(os.environ.get('TOKEN'))

fe1f4135917c00f5072d83a064aeee7e01e7fc27
Y99dr451l <matthias@kockisch.eu>
2022-02-08 00:57:45 +0100

    added activity and status, combined latency with old bot.py commands

--- cogs/misc.py
+++ cogs/misc.py
@@ -3,6 +3,8 @@ from discord.ext import commands
 from discord.ext.commands import is_owner
 import requests
 import json
+import time
+import numpy
 
 class Misc(commands.Cog):
 
@@ -13,11 +15,52 @@ class Misc(commands.Cog):
     async def on_ready(self):
         print(f'Cog {self} loaded.')
 
-    last_channel:str = None
+    rng = numpy.random.default_rng()
+    starttime = time.monotonic()
 
-    @commands.Cog.listener()
-    async def on_member_remove(self, member):
-        print(f'{member} has left a server.')
+    statuses = {
+        'online': discord.Status.online,
+        'idle': discord.Status.idle,
+        'offline': discord.Status.offline,
+        'dnd': discord.Status.dnd,
+    }
+
+    @commands.command()
+    @is_owner()
+    async def status(self, ctx, arg):
+        if arg in self.statuses.keys(): await self.client.change_presence(status=self.statuses[arg])
+        else: await ctx.send(f'`{arg}` is not a valid status.')
+
+    @commands.command()
+    @is_owner()
+    async def activity(self, ctx):
+        await self.client.change_presence(activity=discord.CustomActivity(f'{ctx.message.content[9:]}'))
+
+    @commands.command()
+    async def info(self, ctx):
+        title = f'{self.client.user}'
+        description = f'''
+Owner: {self.client.get_user(self.client.owner_id)}
+Uptime: {self.uptimecalc()}
+Ping: {round(self.client.latency*1000,4)}ms
+Extensions: {str(self.client.extensions.keys())[11:-2]}'''
+        url = 'https://github.com/Y99dr451l/Batbot'
+        embed = discord.Embed(title=title, description=description, url=url, color=self.rng.integers(0, 16777215, dtype=int))
+        embed.set_footer(text=f'Information requested by: {ctx.author.display_name}')
+        await ctx.send(embed=embed)
+    
+    def uptimecalc(self):
+        if not self.starttime: self.starttime = time.monotonic()
+        uptime = time.monotonic()-self.starttime
+        utdys = uptime//(3600*24)
+        uthrs = uptime//3600-utdys*24
+        utmin = uptime//60-uthrs*60-utdys*60*24
+        utsec = uptime-utmin*60-uthrs*3600-utdys*3600*24
+        output = ''
+        if utdys: output += f'{int(utdys)} days, '
+        if uthrs: output += f'{int(uthrs)} hours, '
+        if utmin: output += f'{int(utmin)} minutes, '
+        return output + f'{round(utsec,4)} seconds'
 
     @commands.Cog.listener()
     async def on_message(self, message):
@@ -25,10 +68,6 @@ class Misc(commands.Cog):
             if message.channel.recipient.id != self.client.owner_id:
                 owner = self.client.get_user(self.client.owner_id)
                 await owner.send(message.author.name+' ('+str(message.author.id)+'):\n'+message.content)
-
-    @commands.command(aliases = ['lat', 'ping'])
-    async def latency(self, ctx):
-        await ctx.send(f'{round(self.client.latency*1000,4)}ms')
     
     @commands.command(aliases = ['q'])
     async def quote(self, ctx, apiname = None):

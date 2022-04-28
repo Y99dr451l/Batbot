08bc70984299fba0f09b1a82d97059ef3ab1ac77
Y99dr451l <matthias@kockisch.eu>
2022-04-28 16:39:18 +0200

    fixed failsave condition

--- cogs/image.py
+++ cogs/image.py
@@ -82,7 +82,7 @@ class Image(commands.Cog):
         try: width = int(width); height = int(height); iterations = int(iterations)
         except: await ctx.send('Invalid arguments; expected are `width`, `height`, ìterations` as integers, defaults are 100.'); return
         if width*height*iterations > 10000000: await ctx.send('Arguments too large; their product must be smaller than 1e7'); return
-        if (width or height or iterations) == 0: await ctx.send('Arguments should not be 0.'+(ctx.guild and ctx.guild.get_member('723418507362041876'))*'\n<@723418507362041876> <:pandastare:851850401569701908>'); return
+        if width == 0 or height == 0 or iterations == 0: await ctx.send('Arguments cannot be 0.'+(1 if (ctx.guild and ctx.guild.get_member('723418507362041876')) else 0)*'\n<@723418507362041876> <:pandastare:851850401569701908>'); return
         arr = numpy.random.randint(2, size=(height, width))
         images = []
         images.append(PIL.Image.fromarray(arr*255).convert('L'))
